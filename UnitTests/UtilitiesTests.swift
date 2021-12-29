999bc620c7fc6fead8bea05c1b69c9597d4bf445
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2021-12-29 16:23:12 -0700

    Polish unit tests

--- UnitTests/UtilitiesTests.swift
+++ UnitTests/UtilitiesTests.swift
@@ -3,34 +3,24 @@ import XCTest
 
 final class FormatBirthdateTests: XCTestCase {
     func testConvertsValidBirthdates() {
-        let result = formatBirthdate(date: "1900-01-23")
-        let expected = "01/23/1900"
-        XCTAssertEqual(result, expected)
+        XCTAssertEqual(formatBirthdate(date: "1900-01-23"), "01/23/1900")
     }
 
     func testHasEmptyStringForInvalidFormatting() {
-        let result = formatBirthdate(date: "1-2-1903")
-        let expected = ""
-        XCTAssertEqual(result, expected)
+        XCTAssertEqual(formatBirthdate(date: "1-2-1903"), "")
     }
 
     func testHasEmptyStringForInvalidDates() {
-        let result = formatBirthdate(date: "1900-23-23")
-        let expected = ""
-        XCTAssertEqual(result, expected)
+        XCTAssertEqual(formatBirthdate(date: "1900-23-23"), "")
     }
 }
 
 final class CombineNamesTests: XCTestCase {
     func testHasSpaceBetweenFirstAndLastName() {
-        let result = combineNames(firstName: "Jane", lastName: "Doe")
-        let expected = "Jane Doe"
-        XCTAssertEqual(result, expected)
+        XCTAssertEqual(combineNames(firstName: "Jane", lastName: "Doe"), "Jane Doe")
     }
 
     func testOmitsSpaceWithEmptyLastName() {
-        let result = combineNames(firstName: "Jane", lastName: "")
-        let expected = "Jane"
-        XCTAssertEqual(result, expected)
+        XCTAssertEqual(combineNames(firstName: "Jane", lastName: ""), "Jane")
     }
 }
